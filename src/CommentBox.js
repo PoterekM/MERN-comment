6e8fa34577d8112e227344cbc77af37ace2bdf00
Michelle Poterek <michellep@lumenlearning.com>
2017-11-15 11:54:06 -0800

    remove imported data to the state we created

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -5,19 +5,18 @@ import DATA from './data';
 import style from './style';
 
 class CommentBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { data: [] };
-  }
-  render() {
-    return (
-    <div style={ style.commentBox }>
-    <h2>Comments:</h2>
-    <CommentList data={ DATA }/>
-    <CommentForm />
-    </div>
-    )
-  }
+ constructor(props) {
+ super(props);
+ this.state = { data: [] };
+ }
+ render() {
+ return (
+ <div style={ style.commentBox }>
+ <h2>Comments:</h2>
+ <CommentList data={ this.state.data }/>
+ <CommentForm />
+ </div>
+ )
+ }
 }
-
 export default CommentBox;
